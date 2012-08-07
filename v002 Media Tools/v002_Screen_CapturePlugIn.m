4cd0deeefebf5559344eb20605ca6b724ca88ae5
Douglas Heriot <douglas@douglasheriot.com>
2012-08-07 18:16:48 +1000

    Added try/catch obj-c exception handlers, to prevent crashes! (I don't know enough about CGDisplayStream, IOSurface and OpenGL to fix it properly

--- v002 Media Tools/v002_Screen_CapturePlugIn.m
+++ v002 Media Tools/v002_Screen_CapturePlugIn.m
@@ -59,32 +59,32 @@ static  void MyTextureRelease(CGLContextObj cgl_ctx, GLuint name, void* context)
     
     if([key isEqualToString:@"inputOriginX"])
         return  @{QCPortAttributeNameKey : @"X Origin",
-                QCPortAttributeMinimumValueKey : [NSNumber numberWithUnsignedInteger:0],
-                QCPortAttributeDefaultValueKey : [NSNumber numberWithUnsignedInteger:0]};
-
+		QCPortAttributeMinimumValueKey : [NSNumber numberWithUnsignedInteger:0],
+		QCPortAttributeDefaultValueKey : [NSNumber numberWithUnsignedInteger:0]};
+	
     if([key isEqualToString:@"inputOriginY"])
         return  @{QCPortAttributeNameKey : @"Y Origin",
         QCPortAttributeMinimumValueKey : [NSNumber numberWithUnsignedInteger:0],
         QCPortAttributeDefaultValueKey : [NSNumber numberWithUnsignedInteger:0]};
-
+	
     if([key isEqualToString:@"inputWidth"])
         return  @{QCPortAttributeNameKey : @"Width",
         QCPortAttributeMinimumValueKey : [NSNumber numberWithUnsignedInteger:0],
         QCPortAttributeDefaultValueKey : [NSNumber numberWithUnsignedInteger:mainDisplayRect.size.width]};
-
+	
     if([key isEqualToString:@"inputHeight"])
         return  @{QCPortAttributeNameKey : @"Height",
         QCPortAttributeMinimumValueKey : [NSNumber numberWithUnsignedInteger:0],
         QCPortAttributeDefaultValueKey : [NSNumber numberWithUnsignedInteger:mainDisplayRect.size.height]};
-
-      
+	
+	
 	return nil;
 }
 
 + (NSArray*) sortedPropertyPortKeys
 {
 	return @[@"inputPath",
-             @"outputMovieDidEnd"];
+	@"outputMovieDidEnd"];
 }
 
 + (QCPlugInExecutionMode)executionMode
@@ -137,89 +137,101 @@ static  void MyTextureRelease(CGLContextObj cgl_ctx, GLuint name, void* context)
 
 - (BOOL)execute:(id <QCPlugInContext>)context atTime:(NSTimeInterval)time withArguments:(NSDictionary *)arguments
 {
-    CGLContextObj cgl_ctx = [context CGLContextObj];
-    
-    // Get the current queue we are on.
-    dispatch_queue_t pluginQueue = dispatch_get_current_queue();
-    
-    if([self didValueForInputKeyChange:@"inputDisplayID"])
-    {
-        NSLog(@"new Display ID");
-        
-        // Cleanup existing CGDisplayStream;
-        
-        if(displayStream)
-        {
-            CFRelease(displayStream);
-            displayStream = NULL;
-        }
-        
-        // create a new CGDisplayStream
-        CGDirectDisplayID display = (CGDirectDisplayID) self.inputDisplayID;
-        
-        CGRect bounds = CGDisplayBounds(display);
-        
-        displayStream = CGDisplayStreamCreateWithDispatchQueue(display,
-                                                               bounds.size.width,
-                                                               bounds.size.height,
-                                                               'BGRA',
-                                                               nil,
-                                                               displayQueue,
-                                                               ^(CGDisplayStreamFrameStatus status, uint64_t displayTime, IOSurfaceRef frameSurface, CGDisplayStreamUpdateRef updateRef)
-                                                               {
-                                                                   if(frameSurface)
-                                                                   {
-                                                                       // As per CGDisplayStreams header
-                                                                       CFRetain(frameSurface);
-                                                                       IOSurfaceIncrementUseCount(frameSurface);
-                                                                       
-                                                                       // use the plugins Queue so our GL context is in the correct spot
-                                                                       dispatch_sync(pluginQueue, ^{
-                                                                           
-                                                                           NSUInteger width = IOSurfaceGetWidth(frameSurface);
-                                                                           NSUInteger height = IOSurfaceGetHeight(frameSurface);
-                                                                           
-                                                                           GLuint newTextureForSurface;
-                                                                           
-                                                                           glPushAttrib(GL_TEXTURE_BIT);
-                                                                           
-                                                                           glGenTextures(1, &newTextureForSurface);
-                                                                           glBindTexture(GL_TEXTURE_RECTANGLE_EXT,newTextureForSurface);
-                                                                           
-                                                                           CGLTexImageIOSurface2D(cgl_ctx, GL_TEXTURE_RECTANGLE_EXT, GL_RGBA, width, height, GL_BGRA, GL_UNSIGNED_BYTE, frameSurface, 0);
-                                                                           
-                                                                           glPopAttrib();
-                                                                           
-                                                                           
-                                                                           // make a new
-                                                                           id<QCPlugInOutputImageProvider> provider = nil;
-                                                                           CGColorSpaceRef cspace = CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB);
-                                                                           provider = [context outputImageProviderFromTextureWithPixelFormat:QCPlugInPixelFormatBGRA8
-                                                                                                                                  pixelsWide:width
-                                                                                                                                  pixelsHigh:height
-                                                                                                                                        name:newTextureForSurface
-                                                                                                                                     flipped:YES
-                                                                                                                             releaseCallback:MyTextureRelease
-                                                                                                                              releaseContext:frameSurface
-                                                                                                                                  colorSpace:cspace
-                                                                                                                            shouldColorMatch:YES];
-                                                                           
-                                                                           CGColorSpaceRelease(cspace);
-                                                                           
-                                                                           // set immediately on the plugins queue.
-                                                                           self.displayImageProvider = provider;
-                                                                           
-                                                                       });
-                                                                   }
-                                                               });
-        
-        CGDisplayStreamStart(displayStream);
-        
-    }
-    
-    self.outputImage = self.displayImageProvider;
-    
-    return YES;
+	@try {
+		
+		CGLContextObj cgl_ctx = [context CGLContextObj];
+		
+		// Get the current queue we are on.
+		dispatch_queue_t pluginQueue = dispatch_get_current_queue();
+		
+		if([self didValueForInputKeyChange:@"inputDisplayID"])
+		{
+			NSLog(@"new Display ID");
+			
+			// Cleanup existing CGDisplayStream;
+			
+			if(displayStream)
+			{
+				CFRelease(displayStream);
+				displayStream = NULL;
+			}
+			
+			// create a new CGDisplayStream
+			CGDirectDisplayID display = (CGDirectDisplayID) self.inputDisplayID;
+			
+			CGRect bounds = CGDisplayBounds(display);
+			
+			displayStream = CGDisplayStreamCreateWithDispatchQueue(display, bounds.size.width, bounds.size.height, 'BGRA', nil, displayQueue, ^(CGDisplayStreamFrameStatus status, uint64_t displayTime, IOSurfaceRef frameSurface, CGDisplayStreamUpdateRef updateRef){
+				@try {
+					if(frameSurface)
+					{
+						// As per CGDisplayStreams header
+						CFRetain(frameSurface);
+						IOSurfaceIncrementUseCount(frameSurface);
+						
+						// use the plugins Queue so our GL context is in the correct spot
+						dispatch_sync(pluginQueue, ^{
+							@try {
+								
+								GLsizei width = (GLsizei)IOSurfaceGetWidth(frameSurface);
+								GLsizei height = (GLsizei)IOSurfaceGetHeight(frameSurface);
+								
+								GLuint newTextureForSurface;
+								
+								glPushAttrib(GL_TEXTURE_BIT);
+								
+								glGenTextures(1, &newTextureForSurface);
+								glBindTexture(GL_TEXTURE_RECTANGLE_EXT,newTextureForSurface);
+								
+								CGLTexImageIOSurface2D(cgl_ctx, GL_TEXTURE_RECTANGLE_EXT, GL_RGBA, width, height, GL_BGRA, GL_UNSIGNED_BYTE, frameSurface, 0);
+								
+								glPopAttrib();
+								
+								if(newTextureForSurface)
+								{
+									// make a new
+									id<QCPlugInOutputImageProvider> provider = nil;
+									CGColorSpaceRef cspace = CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB);
+									provider = [context outputImageProviderFromTextureWithPixelFormat:QCPlugInPixelFormatBGRA8
+																						   pixelsWide:width
+																						   pixelsHigh:height
+																								 name:newTextureForSurface
+																							  flipped:YES
+																					  releaseCallback:MyTextureRelease
+																					   releaseContext:frameSurface
+																						   colorSpace:cspace
+																					 shouldColorMatch:YES];
+									
+									CGColorSpaceRelease(cspace);
+									
+									// set immediately on the plugins queue.
+									self.displayImageProvider = provider;
+								}
+								
+							}
+							@catch (NSException *exception) {
+								NSLog(@"Exception updating display stream image provider: %@", exception);
+							}
+						});
+					}
+				}
+				@catch (NSException *exception) {
+					NSLog(@"Exception on display stream update: %@", exception);
+				}
+			});
+			
+			CGDisplayStreamStart(displayStream);
+			
+		}
+		
+		self.outputImage = self.displayImageProvider;
+		
+		return YES;
+	}
+	@catch (NSException *exception) {
+		NSLog(@"Exception executing screen capture: %@", exception);
+		return NO;
+	}
 }
 
 - (void)disableExecution:(id <QCPlugInContext>)context
